fb0fb86c5354da8c127b026f1f3947c116609ba6
onk <takafumi.onaka@gmail.com>
2012-10-07 00:28:31 +0900

    rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yaes::Application.config.secret_token = 'b1fe687b1015ae0fc3f14d1d666c6b13e6b5ff9ea54848bff69fe1f3ea095bcc848da670ea4dddb33df294ad606668f057f5ef680543405ef7f9327d5ab54c46'
