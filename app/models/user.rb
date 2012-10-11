c6db3fae1438a66cc11eda0a829b2348b3098e0d
onk <takafumi.onaka@gmail.com>
2012-10-12 03:45:05 +0900

    annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :integer          not null, primary key
+#  container_user_id :string(255)      not null
+#  nickname          :string(255)      not null
+#  deleted_at        :datetime
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  container_user_id  (container_user_id) UNIQUE
+#
+
 class User < AbstractAppModel
   attr_accessible :container_user_id, :nickname
   acts_as_paranoid
